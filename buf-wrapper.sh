e5ca6b7c4f74b4538cef70bb92f71194e6fe2af3
Johan Norberg <norberg_johan@hotmail.com>
2021-11-12 19:48:45 +0100

    Initial commit with buf 0.56.0

---
+++ buf-wrapper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly VERSION=v0.56.0
+readonly LINUX_X86_64_SHA=48ab010b1328c55d1172b3aa5e1ef6975c12100090b355a026df0e7926947d0b
+
+readonly OS=Linux
+readonly ARCH=x86_64
+
+readonly FILENAME=buf-$OS-$ARCH
+readonly URL=https://github.com/bufbuild/buf/releases/download/$VERSION/$FILENAME
+readonly BINARY_DIR=~/.cache/pre-commit/buf/$OS-$ARCH-$VERSION/
+readonly BINARY=$BINARY_DIR/buf
+
+if [[ -x "$BINARY" ]]; then
+   exec "$BINARY" "$@"
+fi
+
+mkdir -p "$BINARY_DIR"
+TMP_BINARY=$(mktemp)
+
+if ! curl --fail --location --retry 5 --retry-connrefused --silent --output "$TMP_BINARY" "$URL"; then
+   echo "error: failed to download buf" >&2
+   exit 1
+fi
+
+if echo "$LINUX_X86_64_SHA  $TMP_BINARY" | shasum --check --status; then
+   if [[ ! -x "$BINARY" ]]; then
+      mv "$TMP_BINARY" "$BINARY"
+      chmod +x "$BINARY"
+   fi
+
+   exec "$BINARY" "$@"
+else
+   echo "error: buf sha mismatch" >&2
+   rm -f "$BINARY"
+   exit 1
+fi
